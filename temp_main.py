1ad439f83ab7e7f2086655febb422519bf7b5c6e
fromHELIO <146432143+fromHELIO@users.noreply.github.com>
2025-03-25 19:41:10 +0800

    Update temp_main.py - Delete Works Now!
    Added delete inventory functionality and updated existing code to work with database revisions

--- temp_main.py
+++ temp_main.py
@@ -79,55 +79,62 @@ def getEmpId():
 class HomePage(Screen):
     """Home naviagtion page"""
     def endApp(self, *args):
-        """Closes the"""
+        """Closes the app"""
         PinkTabInventory.stop()
         Window.close()
 
 class AddRecord(Screen):
-    """Screen for adding a new inventory item"""
+    """Screen for adding a new inventory record"""
     def addNewInventory(self):
         """adds new item to the database"""
+        itemId = (self.itemId.text).upper()
         name = self.item_name.text
         quantity = self.quantity.text
         exp_date = self.exp_date.text
         price = self.price.text
 
-        try: #checks if quantity is a number
-            int(quantity)
-        except ValueError:
-            popup = Popup(
-                title='Incorrect Quantity',
-                content=Label(text='Please input a whole number.'),
-                size_hint=(None, None), 
-                size=(400, 200)
-                )
-            
+        try:
+            self.idError(itemId)
+        except:
+            popup = Popup(title='Invalid Item ID', content=Label(text='ID must be exactly five letters.'), size_hint=(None, None), size=(400, 200))
             popup.open()
         else:
-            try: #checks if price is a float
-                float(price)
+            try: #checks if quantity is a number
+                int(quantity)
             except ValueError:
-                popup = Popup(title='Invalid Price', content=Label(text='Please input an whole or decimal number.'), size_hint=(None, None), size=(400, 200))
+                popup = Popup(title='Incorrect Quantity', content=Label(text='Please input a whole number.'), size_hint=(None, None), size=(400, 200))
                 popup.open()
             else:
-                try: #checks if date is valid
-                    self.dateError(exp_date)
-                except:
-                    popup = Popup(title='Invalid Date', content=Label(text='Please use the YYYY-MM-DD format.'), size_hint=(None, None), size=(400, 200))
+                try: #checks if price is a float
+                    float(price)
+                except ValueError:
+                    popup = Popup(title='Invalid Price', content=Label(text='Please input an whole or decimal number.'), size_hint=(None, None), size=(400, 200))
                     popup.open()
                 else:
-                    #insert into inventory
-                    cursor.execute(f"INSERT INTO inventory(name, quantity, exp_date, price) VALUES ('{name}', {quantity}, '{exp_date}', {price});")
+                    try: #checks if date is valid
+                        self.dateError(exp_date)
+                    except:
+                        popup = Popup(title='Invalid Date', content=Label(text='Please use the YYYY-MM-DD format.'), size_hint=(None, None), size=(400, 200))
+                        popup.open()
+                    else:
+                        #insert into inventory
+                        cursor.execute(f"INSERT INTO inventory(id, name, quantity, exp_date, price) VALUES ('{itemId.upper()}', '{name}', {quantity}, '{exp_date}', {price});")
 
-                    #insert to changelog
-                    cursor.execute(f"SELECT max(id) FROM inventory")
-                    new_item = cursor.fetchall()[0][0]
+                        #insert to changelog
+                        cursor.execute(f"SELECT max(id) FROM inventory")
+                        new_item = cursor.fetchall()[0][0]
 
-                    cursor.execute(f"INSERT INTO change_log(employee_id, inventory_id, change_type) VALUES({getEmpId()}, {new_item}, 'ADDED INVENTORY')")
+                        cursor.execute(f"INSERT INTO change_log(employee_id, change_type) VALUES('{getEmpId()}', 'ADDED INVENTORY {new_item} - {name}')")
 
-                    # sucess popup
-                    popup = Popup(title='Success!', content=Label(text='The item has been added to the database.'), size_hint=(None, None), size=(400, 200))
-                    popup.open()
+                        # sucess popup
+                        popup = Popup(title='Success!', content=Label(text='The item has been added to the database.'), size_hint=(None, None), size=(400, 200))
+                        popup.open()
+    
+    def idError(self, item_id):
+        if len(item_id) != 5:
+            raise Exception()
+        if not item_id.isalpha():
+            raise Exception()
     
     def dateError(self, date):
         """Tests if date string is valid"""
@@ -141,7 +148,39 @@ class AddRecord(Screen):
                 raise Exception()
             if not(int(testDate[2]) in range(1, 32)):
                 raise Exception()
-                    
+            
+class DeleteRecord(Screen):
+    """Screen for deleting inventory records"""
+    def deleteInventory(self):
+
+        itemId = (self.itemId.text).upper()
+
+        try:
+            self.validateItemID(itemId)
+        except:
+            popup = Popup(title='Invalid Item ID', content=Label(text='Please input a valid input ID.'), size_hint=(None, None), size=(400, 200))
+            popup.open()
+        else:
+            #insert to changelog
+            cursor.execute(f"SELECT name FROM inventory where id = '{itemId}'")
+            deleted_Name = cursor.fetchall()[0][0]
+            
+            cursor.execute(f"INSERT INTO change_log(employee_id, change_type) VALUES('{getEmpId()}', 'DELETED INVENTORY {itemId} - {deleted_Name}')")
+
+            #insert into inventory
+            cursor.execute(f"DELETE FROM inventory WHERE id = '{itemId}';")
+
+            popup = Popup(title='Success!', content=Label(text='The item has been deleted from the database.'), size_hint=(None, None), size=(400, 200))
+            popup.open()
+
+    def validateItemID(self, item_id):
+        cursor.execute(f"SELECT * from inventory where id = '{item_id}'")
+        result = cursor.fetchall()
+        print(result)
+        if len(result) == 0:
+            raise Exception()
+                
+        
 PinkTabInventory().run()
 # close db
 pinkTab.close
